67472984545ecd0af618885679ad5ea47a78999f
Vishakhasenthilnathan <vishakhasenthilnathan@gmail.com>
2022-02-23 23:28:03 +0530

    Test 1

--- src/main/java/moviereview/moviereview/MoviereviewApplication.java
+++ src/main/java/moviereview/moviereview/MoviereviewApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan
+@ComponentScan("main.controllers, main.repositories")
 //@EnableJpaRepositories("main.repositories")
 public class MoviereviewApplication {
 
